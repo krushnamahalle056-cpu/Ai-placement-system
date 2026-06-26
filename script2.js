d3c77093fc23b6d2291a833a013a688666daee90
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-06-27 02:46:02 +0530

    codding

--- script2.js
+++ script2.js
@@ -1,1 +1,19 @@
-console.log("my name is krushna");
+let a = 39 , b = 20;
+
+let c = a+b;
+function add(){
+    console.log("addition of two numbers : "+ c)
+};
+
+let d = a-b;
+function sub(){
+    console.log("subtraction of two numbers : " + d);
+};
+
+if(d==3){
+    console.log("something is happen");
+}else{
+    for(let i=1; i<=d; i++){
+        console.log(i+`my name is Krushna`);
+    }
+}
