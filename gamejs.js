679f7233fccf3fb25d56aaada8bf17a862c17c4e
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-27 00:43:23 +0530

    update time

--- gamejs.js
+++ gamejs.js
@@ -6,7 +6,7 @@ let level = 1;
 
 // Naye variables Time aur Score ke liye
 let score = 0;
-let timeLeft = 15; // Har level ke liye 15 seconds
+let timeLeft = 25; // Har level ke liye 15 seconds
 let timerInterval;
 let isGameOver = false;
 
