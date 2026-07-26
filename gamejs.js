b163a5503521e153af9fd73c903344cfdd3d5f4a
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-07-26 23:44:28 +0530

    target signal code add

--- gamejs.js
+++ gamejs.js
@@ -3,3 +3,10 @@ const ctx = canvas.getContext('2d');
 
 let time = 0;
 let level = 1;
+
+// Target Signal (Jo player ko match karna hai)
+let targetSignal = {
+    type: 'sine',
+    freq: 3,
+    amp: 60
+};
