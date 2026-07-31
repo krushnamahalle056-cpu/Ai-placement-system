f5c4294407c2af566035a7f27ce7668df4606b84
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-01 00:02:34 +0530

    cardsData add

---
+++ question.js
@@ -0,0 +1,9 @@
+// Hamare questions aur answers ka data
+
+const cardsData = [
+    { 
+        question: "What is Time Shifting?", 
+        answer: "A signal is delayed or advanced in time. Equation: y(t) = x(t - t0)." 
+    },
+
+];
