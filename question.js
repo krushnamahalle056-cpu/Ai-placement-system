6324d121687bb244d056ea766c4d4b543b93b209
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-08-01 00:06:18 +0530

    add que 2

--- question.js
+++ question.js
@@ -9,5 +9,10 @@ const cardsData = [
         question: "What is Time Scaling?", 
         answer: "Compression or expansion of a signal in time. Equation: y(t) = x(at)." 
     },
+      { 
+        question: "What is a Class B Amplifier?", 
+        answer: "An amplifier where the active device conducts for only 180 degrees (half) of the input cycle." 
+    },
+   
   
 ];
